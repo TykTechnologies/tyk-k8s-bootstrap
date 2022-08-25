80408791f67cb110f1620d464843e69a5788ed67
Andrei <andrei@tyk.io>
2022-08-25 10:17:38 +0300

    Commited untracked files

---
+++ cmd/bootstrap-post/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"tyk/tyk/bootstrap/data"
+	"tyk/tyk/bootstrap/helpers"
+	"tyk/tyk/bootstrap/license"
+	"tyk/tyk/bootstrap/readiness"
+)
+
+func main() {
+	err := data.InitAppDataPostInstall()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	dashboardLicenseKey, err := license.GetDashboardLicense()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	licenseIsValid, err := license.ValidateDashboardLicense(dashboardLicenseKey)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if licenseIsValid {
+		fmt.Println("license is valid")
+	} else {
+		fmt.Println("license is invalid")
+		os.Exit(1)
+	}
+
+	err = readiness.CheckIfDeploymentsAreReady()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	client := http.Client{}
+
+	err = helpers.CheckForExistingOrganisation(client)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = helpers.GenerateCredentials(client)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if data.AppConfig.BootstrapPortal {
+		err = helpers.BoostrapPortal(client)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	if data.AppConfig.OperatorSecretEnabled {
+		err = helpers.BootstrapTykOperatorSecret()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
