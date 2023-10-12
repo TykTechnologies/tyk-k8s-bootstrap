8e3a749077a230f36fca7279b904dda02a9b663c
Burak Sekili <buraksekili@gmail.com>
2023-10-12 18:34:05 +0300

    Fix incorred build names from testing script
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- hack/load_images.sh
+++ hack/load_images.sh
@@ -2,11 +2,11 @@
 
 make build-all
 
-docker build -t tykio/tyk-k8s-bootstrap-pre-delete:testing -f ./.container/image/bootstrap-pre-delete/Dockerfile ./bin &&
-  kind load docker-image tykio/tyk-k8s-bootstrap-pre-delete:testing
+docker build -t tykio/tyk-k8s-bootstrap-pre-install:testing -f ./.container/image/bootstrap-pre-install/Dockerfile ./bin &&
+  kind load docker-image tykio/tyk-k8s-bootstrap-pre-install:testing
 
-docker build -t tykio/tyk-k8s-bootstrap-pre-delete:testing -f ./.container/image/bootstrap-pre-delete/Dockerfile ./bin &&
-  kind load docker-image tykio/tyk-k8s-bootstrap-pre-delete:testing
+docker build -t tykio/tyk-k8s-bootstrap-post:testing -f ./.container/image/bootstrap-post/Dockerfile ./bin &&
+  kind load docker-image tykio/tyk-k8s-bootstrap-post:testing
 
 docker build -t tykio/tyk-k8s-bootstrap-pre-delete:testing -f ./.container/image/bootstrap-pre-delete/Dockerfile ./bin &&
   kind load docker-image tykio/tyk-k8s-bootstrap-pre-delete:testing
