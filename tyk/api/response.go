68de1e3d2ea0c759b76da7da935f5c61be1ec096
Burak Sekili <buraksekili@gmail.com>
2023-11-10 13:47:36 +0300

    Remove unused fields from CreateUserResp structure
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- tyk/api/response.go
+++ tyk/api/response.go
@@ -1,7 +1,5 @@
 package api
 
-import "time"
-
 type DashboardAPIResp struct {
 	Status  string `json:"Status"`
 	Message string `json:"Message"`
@@ -17,21 +15,6 @@ type CreateUserResp struct {
 	Status  string `json:"Status"`
 	Message string `json:"Message"`
 	Meta    struct {
-		APIModel        struct{} `json:"api_model"`
-		FirstName       string   `json:"first_name"`
-		LastName        string   `json:"last_name"`
-		EmailAddress    string   `json:"email_address"`
-		OrgID           string   `json:"org_id"`
-		Active          bool     `json:"active"`
-		ID              string   `json:"id"`
-		AccessKey       string   `json:"access_key"`
-		UserPermissions struct {
-			IsAdmin string `json:"IsAdmin"`
-		} `json:"user_permissions"`
-		GroupID         string        `json:"group_id"`
-		PasswordMaxDays int           `json:"password_max_days"`
-		PasswordUpdated time.Time     `json:"password_updated"`
-		PWHistory       []interface{} `json:"PWHistory"`
-		CreatedAt       time.Time     `json:"created_at"`
+		ID string `json:"id"`
 	} `json:"Meta"`
 }
